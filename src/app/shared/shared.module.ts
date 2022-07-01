b0be723fbba4ea7df6606d72a15de75cb9864b24
Floyd Fernandes <floydfernandes20@gmail.com>
2022-07-01 20:36:11 +0530

    Added Angular Material.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { HeaderComponent } from './components/layout/header/header.component';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule],
   exports: [HeaderComponent],
 })
 export class SharedModule {}
