6817011c83d880f655b891797d0a8b1d50fe0e95
Floyd Fernandes <floydfernandes20@gmail.com>
2022-07-02 12:31:14 +0530

    Added bootstrap, fonts and implemented header design.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 import { HeaderComponent } from './components/layout/header/header.component';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, MatToolbarModule],
+  imports: [CommonModule, MatIconModule],
   exports: [HeaderComponent],
 })
 export class SharedModule {}
