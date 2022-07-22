c0788b2406cff6c9e06cfe8ee83d5d255d3252b8
Floyd Fernandes <floydfernandes20@gmail.com>
2022-07-22 11:58:28 +0530

    Added service worker.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ContactUsComponent } from './core/components/contact-us/contact-us.component';
 import { AboutComponent } from './core/components/about/about.component';
 import { BlogComponent } from './core/components/blog/blog.component';
@@ -11,6 +11,8 @@ import { HomeModule } from './core/modules/home/home.module';
 import { AppRoutingModule } from './app-routing.module';
 import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
 import { LoginComponent } from './login/login.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,19 @@ import { LoginComponent } from './login/login.component';
     PageNotFoundComponent,
     LoginComponent,
   ],
-  imports: [BrowserModule, SharedModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    FormsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
